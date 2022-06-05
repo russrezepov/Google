a6fc2809d6ce9d936195b48d84156fe9c2c8df7c
Progrr <russr2017@gmail.com>
2022-06-04 22:54:07 -0400

    BinaryTree Search Insert Delete Traverse

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Random;
 
     public class BinaryTree {
@@ -80,6 +82,104 @@ import java.util.Random;
 
             return bt;
         }
+
+        private boolean containsNodeRecursive(Node current, int value) {
+            if (current == null) {
+                return false;
+            }
+            if (value == current.value) {
+                return true;
+            }
+            return value < current.value
+                    ? containsNodeRecursive(current.left, value)
+                    : containsNodeRecursive(current.right, value);
+        }
+
+        public boolean containsNode(int value) {
+            return containsNodeRecursive(root, value);
+        }
+
+//        Another common operation is the deletion of a node from the tree.
+//        First, we have to find the node to delete in a similar way as before:
+
+        private Node deleteRecursive(Node current, int value) {
+            if (current == null) {
+                return null;
+            }
+
+            if (value == current.value) {
+
+                //LEAF NODE
+                if (current.left == null && current.right == null) {
+                    return null;
+                }
+
+                //ONE CHILD
+                if (current.right == null) {
+                    return current.left;
+                }
+
+                if (current.left == null) {
+                    return current.right;
+                }
+
+                //Finally, we have to handle the case where the node has two children.
+
+                //First, we need to find the node that will replace the deleted node.
+                // We'll use the smallest node of the soon to be deleted node's right sub-tree:
+
+
+                    int smallestValue = findSmallestValue(current.right);
+                    current.value = smallestValue;
+                    current.right = deleteRecursive(current.right, smallestValue);
+                    return current;
+
+
+                //Then we assign the smallest value to the node to delete, and after that, we'll delete it from the right sub-tree:
+            }
+            if (value < current.value) {
+                current.left = deleteRecursive(current.left, value);
+                return current;
+            }
+
+            current.right = deleteRecursive(current.right, value);
+            return current;
+        }
+        private int findSmallestValue(Node root) {
+            return root.left == null ? root.value : findSmallestValue(root.left);
+        }
+//        Once we find the node to delete, there are 3 main different cases:
+//        a node has no children – this is the simplest case; we just need to replace this node with null in its parent node
+//        a node has exactly one child – in the parent node, we replace this node with its only child.
+//        a node has two children – this is the most complex case because it requires a tree reorganization
+
+        public void deleteNode(int value) {
+            root = deleteRecursive(root, value);
+        }
+
+        public void BFS() {
+            if (root == null) {
+                return;
+            }
+
+            Queue<Node> nodes = new LinkedList<>();
+            nodes.add(root);
+
+            while (!nodes.isEmpty()) {
+
+                Node node = nodes.remove();
+
+                System.out.print(" " + node.value);
+
+                if (node.left != null) {
+                    nodes.add(node.left);
+                }
+
+                if (node.right != null) {
+                    nodes.add(node.right);
+                }
+            }
+        }//BFS
 
     public static void main(String[] args) {
 
